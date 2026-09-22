1cbdc3f869d6e702b06696668cd883abe5d30d47
MarcusOrion <manvati.marco@gmail.com>
2026-09-22 11:45:58 +0200

    add footer and minor changes to main

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Jumbotron from "./components/Jumbotron";
 import Main from "./components/Main";
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Jumbotron />
       <Main />
+      <Footer />
     </>
   );
 }
---
+++ react-dc-comics/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800">
+      <div className="flex justify-center items-center h-13 w-60 bg-blue-600 py-4 px-6 border-2 border-blue-600">
+        <h1 className="text-white text-center font-semibold uppercase">
+          Sign-up now!
+        </h1>
+      </div>
+    </footer>
+  );
+}
