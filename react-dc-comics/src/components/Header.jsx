7466e912ba9a78cc73a93fd0e66ad6ca7ed0089c
MarcusOrion <manvati.marco@gmail.com>
2026-09-22 10:55:00 +0200

    add minor adjustments to header and jumbotron

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import logo from "../assets/dc-logo.png";
 export default function Header() {
   return (
-    <header className="flex justify-between items-center px-8 py-4 bg-white">
+    <header className="flex justify-between items-center ml-[200px] px-8 py-4 bg-white">
       <div>
-        <img className="align-start" src={logo} alt="Logo della DC Comics" />
+        <img
+          className="flex items-center"
+          src={logo}
+          alt="Logo della DC Comics"
+        />
       </div>
-      <ul className="flex items-center gap-6 list-none font-semibold text-sm uppercase text-gray-700">
+      <ul className="flex items-center gap-6 list-none font-bold text-sm uppercase text-gray-700">
         <li>Characters</li>
         <li>Comics</li>
         <li>Movies</li>
--- react-dc-comics/src/components/Jumbotron.jsx
+++ react-dc-comics/src/components/Jumbotron.jsx
@@ -1,7 +1,7 @@
 import jumbotron from "../assets/jumbotron.jpg";
 export default function Jumbotron() {
   return (
-    <div className="relative w-full h-[350px] overflow-hidden">
+    <div className="relative w-full h-[550px] overflow-hidden">
       <img
         className="w-full h-full object-cover object-top"
         src={jumbotron}
