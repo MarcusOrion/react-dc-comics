39a9a3f34e564e38dd70976f1fe1edc22caa3ba2
MarcusOrion <manvati.marco@gmail.com>
2026-09-23 16:06:18 +0200

    add new color to current series and moad more (header.jsx)

--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -4,14 +4,14 @@ export default function Main() {
   return (
     <main className="bg-black flex flex-col items-center">
       <section>
-        <div className="flex relative bottom-0 left-100 -translate-y-1/2 justify-center items-center h-13 w-60 bg-blue-600 py-4 px-6">
+        <div className="flex relative bottom-0 left-25 -translate-y-1/2 justify-center items-center h-13 w-60 bg-[#0076de] py-4 px-6">
           <h1 className="text-white text-center font-bold uppercase text-xl">
             Current Series
           </h1>
         </div>
         <ProductList />
       </section>
-      <div className="flex justify-center align-center items-center h-13 w-60 bg-blue-600 py-4 px-6 mb-5 mt-8">
+      <div className="flex justify-center align-center items-center h-11 w-60 bg-[#0076de] py-4 px-6 mb-5 mt-[100px]">
         <h2 className="h1 text-white font-bold uppercase text-x1">Load More</h2>
       </div>
     </main>
